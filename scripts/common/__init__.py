8285cb40528a61e55e177b506445f92c73469215
capsaicine-dev <remzanzi@gmail.com>
2026-01-23 01:11:02 +0100

    [Major] Remove python scripts

--- scripts/common/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (C) 2026 zizanibot
-# See LICENSE file for extended copyright information.
-# This file is part of MyDeputeFr project from https://github.com/zizanibot/InterpelMail.
--- scripts/main.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-from download.update import update
-
-
-if __name__ == "__main__":
-    asyncio.run(update())
